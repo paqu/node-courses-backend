0665a7eeb88fdb9abcd0b9fb9712fb22c324c05d
paqu <pawel.kusz1993@gmail.com>
2021-03-23 02:26:32 +0100

    Create simple logger as middleware

--- server.js
+++ server.js
@@ -4,13 +4,17 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config/config.env" });
 const PORT = process.env.PORT || 4000;
 
+// Middleware
+const logger = require("./middleware/logger");
+
 // Route files
 const bootcamps = require("./routes/bootcamps");
 
 const app = express();
 
-// Mount routers
+app.use(logger);
 app.use("/api/v1/bootcamps", bootcamps);
+
 app.get("/", (req, res) => {
   res.send("Hello world");
   res.end();
