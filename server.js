2f89072e5207ecba3e70e1fb7c4dc0a2679625d5
paqu <pawel.kusz1993@gmail.com>
2021-03-23 01:10:06 +0100

    Add config to project

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config/config.env" });
 const app = express();
-const port = 5000;
+
+const PORT = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}.`
+  );
 });
