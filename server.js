1276421510a7c380814203521078158eddb241d0
paqu <pawel.kusz1993@gmail.com>
2021-03-23 01:37:37 +0100

    Create first routes for bootcamps

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require("express");
 const dotenv = require("dotenv");
 
 dotenv.config({ path: "./config/config.env" });
-const app = express();
-
 const PORT = process.env.PORT || 4000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// Route files
+const bootcamps = require("./routes/bootcamps");
+
+const app = express();
+
+// Mount routers
+app.use("/api/v1/bootcamps", bootcamps);
 
 app.listen(PORT, () => {
   console.log(
