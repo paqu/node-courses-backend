f520cb69f7d4947269be3f26ead3e32a1d04cdf4
paqu <pawel.kusz1993@gmail.com>
2021-03-28 14:57:02 +0200

    Implement get action handling for bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -13,10 +13,16 @@ exports.getBootcamps = (req, res, next) => {
     @route  GET /api/v1/bootcamps/:id
     @access Public
 */
-exports.getBootcamp = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Show bootcamp ${req.params.id}.` });
+exports.getBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+
+    if (!bootcamp) res.status(400).json({ success: false });
+
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 /*
