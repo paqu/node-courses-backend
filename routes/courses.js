c016af96a704a561081188c8ec9139ad93c1ec62
paqu <pawel.kusz1993@gmail.com>
2021-04-02 00:59:23 +0200

    Get single course

--- routes/courses.js
+++ routes/courses.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
-const { getCourses } = require("../controllers/courses");
+const { getCourses, getCourse } = require("../controllers/courses");
 
 router.route("/").get(getCourses);
+router.route("/:id").get(getCourse);
 
 module.exports = router;
